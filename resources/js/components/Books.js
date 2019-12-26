918e41c6f0cb07ade5884d3953aacc5ae5795e72
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2019-12-26 15:09:03 +0100

    Style books component

--- resources/js/components/Books.js
+++ resources/js/components/Books.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
 
+import './css/Books.css';
+
 const list = [
     {
         title: 'React',
@@ -22,6 +24,16 @@ const list = [
 
 const onDismiss = (item) => this.onDismiss(item);
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+const largeColumn = {
+    width: '40%',
+};
+const midColumn = {
+    width: '30%',
+};
+const smallColumn = {
+    width: '10%',
+};
 
 class Books extends Component {
 
@@ -50,10 +62,12 @@ class Books extends Component {
         const {searchTerm, list} = this.state;
         return (
             <div className="books">
-                <Search value={searchTerm}
-                        onChange={this.onSearchChange} >
-                    Search
-                </Search>
+                <div className="interactions">
+                    <Search value={searchTerm}
+                            onChange={this.onSearchChange} >
+                        Search
+                    </Search>
+                </div>
                 <Table list={list}
                        pattern={searchTerm}
                        onDismiss={this.onDismiss} />
@@ -70,19 +84,20 @@ const Search = ({value, onChange, children}) =>
     </form>
 
 const Table = ({list, pattern, onDismiss}) =>
-    <div>
+    <div className="table">
         {list.filter(isSearched(pattern))
             .map(item => {
                 return (
-                    <div key={item.objectID}>
-                        <span>{item.objectID}</span>
-                        <span><a href={item.url}>{item.title}</a></span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                        <span>
+                    <div key={item.objectID} className="table-row">
+                        <span style={{ width: '40%'}}>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span style={{ width: '30%'}}>{item.author}</span>
+                        <span style={{width: '10%'}}> {item.num_comments}</span>
+                        <span style={{width: '10%'}}>{item.points}</span>
+                        <span style={{width: '10%'}}>
                             <Button onClick={() => onDismiss(item.objectID)}
-                                    >Dismiss</Button>
+                                    className="button-inline">Dismiss</Button>
                         </span>
                     </div>
                 )
