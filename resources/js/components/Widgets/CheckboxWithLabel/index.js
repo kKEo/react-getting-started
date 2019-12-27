9e18ec899bf6c88548b5fd9912bffb9ee73bb3f3
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2019-12-27 19:13:21 +0100

    Add checkbox with dynamic label

---
+++ resources/js/components/Widgets/CheckboxWithLabel/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class CheckboxWithLabel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {isChecked: false}
+    }
+
+    onChange = () => {
+        this.setState({isChecked: !this.state.isChecked});
+    };
+
+    render() {
+        return (
+            <label ref={this.props.labelRef}>
+                <input
+                    ref={this.props.inputRef}
+                    type="checkbox"
+                    checked={this.state.isChecked}
+                    onChange={this.onChange}
+                    />
+                {this.state.isChecked ?
+                    this.props.labelOn :
+                    this.props.labelOff}
+            </label>
+        );
+    }
+}
