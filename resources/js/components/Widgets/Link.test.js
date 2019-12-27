fbb25b5208b846954c4e489e4daaebc9e0928481
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2019-12-27 17:50:01 +0100

    Add most basic 'renders correctly' test

--- resources/js/components/Widgets/Link.test.js
+++ resources/js/components/Widgets/Link.test.js
@@ -2,3 +2,9 @@ import React from 'react';
 import Link from './Link';
 import renderer from 'react-test-renderer';
 
+it ('renders correctly', () => {
+    const tree = renderer
+        .create(<Link page="https://centrumrekodziela.pl">Centrum</Link>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
