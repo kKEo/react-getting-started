318a233ed3276604640d2936981c672afcaca075
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-01-01 15:02:53 +0100

    Move reducers to separate source file

--- resources/js/components/Todos/index.js
+++ resources/js/components/Todos/index.js
@@ -3,39 +3,7 @@ import React from "react";
 import {createStore, combineReducers} from "redux";
 import {connect, Provider} from "react-redux";
 
-const todo = (state, action) => {
-    switch(action.type) {
-        case 'ADD_TODO':
-            return {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                };
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
-            return {
-                ...state,
-                completed: !state.completed
-            }
-    }
-};
-
-const todos = (state =[], action) => {
-    switch(action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ];
-        case 'TOGGLE_TODO':
-            return state.map(t => todo(t, action))
-        default:
-            return state;
-    }
-};
-
+import todoApp from "./Reducers";
 
 let nextTodoId = 0;
 const addTodo = (text) => ({
@@ -53,23 +21,6 @@ const toggleTodo = (id) => ({
         type: 'TOGGLE_TODO',
         id
     });
-
-const visibilityFilter = (
-    state = 'SHOW_ALL',
-    action
-) => {
-    switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-            return action.filter;
-        default:
-            return state;
-    }
-};
-
-const todoApp = combineReducers ({
-    todos,
-    visibilityFilter
-});
 
 const Link = ({
     active, children, onClick
