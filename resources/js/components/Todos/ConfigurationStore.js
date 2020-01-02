c5e03f3dd25bae17c124107d2445ed4e7e005ad3
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-01-02 22:54:44 +0100

    Add some logging on prev and current state

--- resources/js/components/Todos/ConfigurationStore.js
+++ resources/js/components/Todos/ConfigurationStore.js
@@ -4,11 +4,30 @@ import throttle from "lodash/throttle";
 import {createStore} from "redux";
 import todoApp from "./Reducers";
 
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+    if (!console.group) {
+        return rawDispatch;
+    }
+
+    return (action) => {
+        console.group(action.type);
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue',action);
+        const returnValue = rawDispatch;
+        console.log('%c next state', 'color: green', store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    }
+}
+
 const configureStore = () => {
     const persistedState = loadState();
     const store = createStore(
         todoApp, persistedState
     );
+
+    store.dispatch = addLoggingToDispatch(store);
 
     store.subscribe(throttle(() => {
         saveState({
